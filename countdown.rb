9f6141df4deeb98ce0aefe02be6ec40a5233a426
Ibrahim  Akar <ibsakar@gmail.com>
2019-09-30 21:24:45 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,11 @@
 #write your code here
 
 def countdown(num)
-  num = 10
+
   while num >= 1
   puts "#{x} SECOND(S)!"
-  num = num - 1
+  num = num + 1 
+  break if num == 10
  end
  puts "HAPPY NEW YEAR!"
 end 
